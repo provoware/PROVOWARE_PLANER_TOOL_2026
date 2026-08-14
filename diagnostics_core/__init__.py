1a38340829ed9b136dc8205275bdad4559db7f3b
pppoppi <provoware.one@gmail.com>
2026-08-14 14:36:19 +0200

    I012: Diagnose-Domainmodell anlegen

---
+++ diagnostics_core/__init__.py
@@ -0,0 +1,5 @@
+"""I012-Diagnosemodelle für read-only Zustandsauswertung."""
+
+from diagnostics_core.model import DiagnosisItem, DiagnosisSnapshot, DiagnosisState
+
+__all__ = ["DiagnosisItem", "DiagnosisSnapshot", "DiagnosisState"]
