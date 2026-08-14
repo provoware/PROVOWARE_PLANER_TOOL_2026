37026de57c985d0f9df03ccaffe7ae6fd2a4cff4
pppoppi <provoware.one@gmail.com>
2026-08-14 06:02:19 +0200

    I008: Synchronisations-Vorschau-Domain anlegen

---
+++ sync_core/__init__.py
@@ -0,0 +1,13 @@
+from .model import (
+    SyncFieldAction,
+    SyncFieldPreview,
+    SyncPreview,
+    SyncPreviewState,
+)
+
+__all__ = [
+    "SyncFieldAction",
+    "SyncFieldPreview",
+    "SyncPreview",
+    "SyncPreviewState",
+]
